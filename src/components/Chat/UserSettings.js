1b0e2d6fc12ae8bc5912ee5911fd2e46375d1187
yanalshmilan <yshmilan@gmail.com>
2021-07-27 20:58:43 +0300

    added create room

--- src/components/Chat/UserSettings.js
+++ src/components/Chat/UserSettings.js
@@ -140,7 +140,7 @@ export default function UserSettings() {
         <Disclosure as="div" className="mt-2">
           {({ open }) => (
             <>
-              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-purple-900 bg-purple-100 rounded-lg hover:bg-indigo-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left text-white bg-indigo-600 rounded-lg hover:bg-indigo-400 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
                 <span>Chat Settings</span>
               </Disclosure.Button>
               <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
