682a0aeebc13d9ff15c5a9ef904bdc10d3f978ac
yanalshmilan <yshmilan@gmail.com>
2021-07-28 13:28:38 +0300

    test deployed

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,29 +1,29 @@
-import React, { useEffect, useState } from "react";
-import ContactList from "./ContactList";
-import InputField from "./InputField";
-import LeftHeader from "./LeftHeader";
-import MsgsList from "./MsgsList";
-import RightHeader from "./RightHeader";
-import SearchBar from "./SearchBar";
-import { io } from "socket.io-client";
-import Room from "./Room";
-import { useDispatch, useSelector } from "react-redux";
-import { addMessage } from "../../store/actions/chatActions";
+import React, { useEffect, useState } from 'react';
+import ContactList from './ContactList';
+import InputField from './InputField';
+import LeftHeader from './LeftHeader';
+import MsgsList from './MsgsList';
+import RightHeader from './RightHeader';
+import SearchBar from './SearchBar';
+import { io } from 'socket.io-client';
+import Room from './Room';
+import { useDispatch, useSelector } from 'react-redux';
+import { addMessage } from '../../store/actions/chatActions';
 export default function Chat() {
   const user = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
   const [socket, setSocket] = useState(false);
   const [loading, setLoading] = useState(false);
   useEffect(() => {
-    setSocket(io("localhost:8000"));
+    setSocket(io('http://34.141.93.52:8000'));
   }, []);
   if (socket && loading === false) {
     setLoading(true);
   }
   useEffect(() => {
     if (socket) {
-      socket.emit("userId", user._id);
-      socket.on("message", (message) => {
+      socket.emit('userId', user._id);
+      socket.on('message', (message) => {
         // to Do , add sound
         dispatch(addMessage(message.roomId, message.content));
       });
@@ -36,9 +36,9 @@ export default function Chat() {
     <>
       {chats && (
         <div>
-          <div class="w-full h-32" style={{ backgroundColor: "#312E81" }}></div>
+          <div class="w-full h-32" style={{ backgroundColor: '#312E81' }}></div>
 
-          <div class="container mx-auto" style={{ marginTop: "-128px" }}>
+          <div class="container mx-auto" style={{ marginTop: '-128px' }}>
             <div class="py-6 h-screen">
               <div class="flex border border-grey rounded shadow-lg h-full">
                 {/* <!-- Left --> */}
